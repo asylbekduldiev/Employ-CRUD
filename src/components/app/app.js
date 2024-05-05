af510d15164ae776191e351d229bc0bfc336dd30
Asyl <duldievasylbek@gmail.com>
2024-05-05 21:49:14 +0500

    Add&delete

---
+++ src/components/app/app.js
@@ -0,0 +1,74 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
+            ]
+        }
+        this.maxId = 4;
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+ 
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+    onToogleIncrease = (id) => {
+      console.log(`Increase this ${id}`);
+    }
+    onToogleRise = (id) => {
+      console.log(`Rise this ${id}`)
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                    onToogleIncrease={this.onToogleIncrease}
+                    onToogleRise={this.onToogleRise}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
+}
+
+export default App;
