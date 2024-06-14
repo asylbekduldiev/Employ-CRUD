263d2f55e3c40c23371210ae3e549530ca2bc763
Asyl <duldievasylbek@gmail.com>
2024-06-15 00:41:02 +0500

    Prop-types

--- src/components/Dashboard/employees-list/employees-list.jsx
+++ src/components/Dashboard/employees-list/employees-list.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import EmployeesListItem from "../employees-list-item/employees-list-item";
-
 import './employees-list.css';
 
-const EmployeesList = ({data, onDelete, onToggleProp}) => {
-
-    const elements = data.map(item => {
-        const {id, ...itemProps} = item;
-        return (
-            <EmployeesListItem 
-                key={id} 
-                {...itemProps}
-                onDelete={() => onDelete(id)}
-                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
-        )
-    })
-
+const EmployeesList = ({ data, onDelete, onToggleProp }) => {
+  const elements = data.map(item => {
+    const { id, ...itemProps } = item;
     return (
-        <ul className="app-list list-group">
-            <li>
-            {elements}
-            </li>
-        </ul>
-    )
-}
+      <EmployeesListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => onDelete(id)}
+        onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+      />
+    );
+  });
+
+  return (
+    <ul className="app-list list-group">
+      {elements}
+    </ul>
+  );
+};
+
+EmployeesList.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onToggleProp: PropTypes.func.isRequired,
+};
 
 export default EmployeesList;
